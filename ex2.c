638494164b47a4780382693d3d914693d74de5e2
Master <83012647+MaxMuradov@users.noreply.github.com>
2024-11-26 15:58:02 +0200

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -8,16 +8,7 @@ Assignment: ex2
 
 
 int main() {
-	/*
-		// Case 1: Draw Happy Face with given symbols for eyes, nose and mouse
-		/* Example:
-		* n = 3:
-		* 0   0
-		*   o
-		* \___/
-		*/
 
-	//check input
 	char eye, mouth, nose;
 	int menu, num, sum, face_size, prime, chain_num;
 
@@ -61,16 +52,6 @@ int main() {
 
 			break;
 
-
-			// Case 2: determine whether the sum of all digits to the left of the middle digit(s)
-			// and the sum of all digits to the right of the middle digit(s) are equal
-			/* Examples:
-			Balanced: 1533, 450810, 99
-			Not blanced: 1552, 34
-			Please notice: the number has to be bigger than 0.
-			*/
-
-			//check input
 			case 2:
 				int i;
 				sum = 0;
@@ -103,18 +84,6 @@ int main() {
 
 			break;
 
-
-
-
-
-
-			// Case 3: determine whether the sum of the proper divisors (od an integer) is greater than the number itself
-			/* Examples:
-			Abudant: 12, 20, 24
-			Not Abudant: 3, 7, 10
-			Please notice: the number has to be bigger than 0.
-			*/
-
 			case 3:
 				sum = 0;
 				printf("Enter a number: \n");
@@ -133,15 +102,6 @@ int main() {
 					printf("This number does not share.\n");
 			break;
 
-
-			// Case 4: determine wether a number is a prime.
-			/* Examples:
-			This one brings joy: 3, 5, 11
-			This one does not bring joy: 15, 8, 99
-			Please notice: the number has to be bigger than 0.
-			*/
-
-			//check + 1!
 			case 4:
 				prime = 0;
 				printf("Enter a number: \n");
@@ -164,12 +124,6 @@ int main() {
 					printf("The circle remains incomplete.\n");
 			break;
 
-			// Happy numbers: Print all the happy numbers between 1 to the given number.
-			// Happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit
-			/* Examples:
-			Happy :) : 7, 10
-			Not Happy :( : 5, 9
-			Please notice: the number has to be bigger than 0.*/
 			case 5:
 				printf("Enter a number: \n");
 				scanf("%d", &num);
@@ -196,19 +150,6 @@ int main() {
 			printf("\n");
 			break;
 
-
-
-
-			// Festival of Laughter: Prints all the numbers between 1 the given number:
-			// and replace with "Smile!" every number that divided by the given smile number
-			// and replace with "Cheer!" every number that divided by the given cheer number
-			// and replace with "Festival!" every number that divided by both of them
-			/* Example:
-			6, smile: 2, cheer: 3 : 1, Smile!, Cheer!, Smile!, 5, Festival!
-			*/
-
-			// char str[128];
-			// char key[128] = {s,m,i,l,e,':', ,c,h,e,e,r,':'};
 			case 6:
 				int smile, cheer, max;
 				printf("Enter a number: \n");
